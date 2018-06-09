823e3ae845d352eeee17ed5b198ebb4c2b737089
Mariam <MariamAbdelmohsen@users.noreply.github.com>
2018-06-08 19:53:58 -0700

    nav and header
    Nav and header

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 import './graphql/login.js';
 import './graphql/posts.js';
+
+(function($) { // Begin jQuery
+
+    $(document).ready(function() {
+        var menu = $('nav > ul'),
+            menuLink = $('#mobile-menu a');
+        menuLink.on('click', function() {
+            if (menu.hasClass('closed')) {
+                menu.removeClass('closed');
+                menu.addClass('open');
+            } else {
+                menu.removeClass('open');
+                menu.addClass('closed');
+            }
+        });
+    });
+    date1 = new Date();
+    date = date1.getDate();
+    year = date1.getFullYear();
+    month = date1.getMonth();
+
+})(jQuery);
--- js/build/app.js
+++ js/build/app.js
@@ -10647,3 +10647,25 @@ $(document).ready(function () {
         });
     }
 });
+
+(function ($) {
+    // Begin jQuery
+
+    $(document).ready(function () {
+        var menu = $('nav > ul'),
+            menuLink = $('#mobile-menu a');
+        menuLink.on('click', function () {
+            if (menu.hasClass('closed')) {
+                menu.removeClass('closed');
+                menu.addClass('open');
+            } else {
+                menu.removeClass('open');
+                menu.addClass('closed');
+            }
+        });
+    });
+    date1 = new Date();
+    date = date1.getDate();
+    year = date1.getFullYear();
+    month = date1.getMonth();
+})(jQuery);
