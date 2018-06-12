43f33c70915ef6276ca482b0c6c5d9c4b044f774
Catherine Blake Smith <catherine.b.smith@seattlecentral.edu>
2018-06-12 22:55:00 +0000

    Finalizes styles for site as much as possible. index.php is complete. There are still some items to finish: footer placement, dynamic footer with php, and possibly including bootstrap classes for daily and detail in order to have consistent styling throughout that is not reliant on floats. can this be done with JavaScript? Also have question about $ appearing from database and want to tighten up the form widths.

--- item_detail.php
+++ item_detail.php
@@ -1,7 +1,7 @@
 <?php include 'includes/header.php'?>
     <div class="container-fluid" id="detail">
         <div class="row">
-            <div class="col-lg-8 col-md-8 col-sm-12">
+            <div class="col-lg-10 col-md-10 col-sm-12">
                 <h2 id="item-title" class="title"></h2>
                 <div id="item-image">
                     Loading...
@@ -15,6 +15,12 @@
             </div>
         </div>
     </div>
+    <footer>
+        <div id="copyright">&copy; <?=$year?> MimiSmith</div>
+        <div id="github"><a href="http://www.github.com/thecbsnetwork/web130_final" target="_blank">
+        <i class="fab fa-github-square"> </i>Read the code on Github</a></div>
+        <div id="seattle">Seattle, WA</div>
+    </footer>
 <script type="text/javascript">
     var JS_PAGE = 'detail_view';
 </script>
